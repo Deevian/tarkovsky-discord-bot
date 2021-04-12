878f6f82bc1c359e09ebf4c0d7d534443f717d49
João Abreu <jpmcabreu@gmail.com>
2021-04-13 00:21:09 +0100

    Implement initial pass for slash commands

--- src/index.js
+++ src/index.js
@@ -1,92 +1,50 @@
 const fs = require("fs");
-const path = require("path");
 const Discord = require("discord.js");
 const config = require("../config.json");
+const noop = require("./commands/utility/noop");
 
 const setupClient = client => {
 	client.commands = new Discord.Collection();
-	client.cooldowns = new Discord.Collection();
 
-	const commandFiles = fs.readdirSync(path.resolve(__dirname, './commands'))
-		.filter(file => file.endsWith('.js'));
-	for (const file of commandFiles) {
-		const command = require(path.resolve(__dirname, `./commands/${file}`));
-		client.commands.set(command.name, command);
-	}
+	fs.readdirSync(`${__dirname}/commands`).forEach(folder => {
+		const commandFiles = fs.readdirSync(`${__dirname}/commands/${folder}`)
+			.filter(file => file.endsWith('.js'));
+
+		commandFiles.forEach(file => {
+			const command = require(`${__dirname}/commands/${folder}/${file}`);
+
+			client.commands.set(`${folder}__${file.replace(".js", "")}`, command);
+		})
+	})
 
 	return client;
 }
 
-const onMessage = (client, message) => {
-	if (!message.content.startsWith(config.prefix) || message.author.bot) return;
-
-	const args = message.content.slice(config.prefix.length).trim().split(/ +/);
-	const commandName = args.shift().toLowerCase();
-
-	const command = client.commands.get(commandName)
-		|| client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
-
-	if (!command) return;
-
-	if (command.guildOnly && message.channel.type === 'dm') {
-		return message.reply('I can\'t execute that command inside DMs!');
+const onInteraction = (client, interaction) => {
+	const parent = interaction.data.options[0];
+	if (!parent) {
+		return noop(client, interaction);
 	}
 
-	if (command.permissions) {
-		const authorPerms = message.channel.permissionsFor(message.author);
-		if (!authorPerms || !authorPerms.has(command.permissions)) {
-			return message.reply('You can not do this!');
-		}
+	const child = parent.options[0];
+	if (!child) {
+		return noop(client, interaction);
 	}
 
-	if (command.args && !args.length) {
-		let reply = `You didn't provide any arguments, ${message.author}!`;
-
-		if (command.usage) {
-			reply += `\nThe proper usage would be: \`${config.prefix}${command.name} ${command.usage}\``;
-		}
-
-		return message.channel.send(reply);
+	const command = client.commands.get(`${parent.name}__${child.name}`)
+	if (!command) {
+		return noop(client, interaction);
 	}
 
-	const { cooldowns } = client;
-
-	if (!cooldowns.has(command.name)) {
-		cooldowns.set(command.name, new Discord.Collection());
-	}
-
-	const now = Date.now();
-	const timestamps = cooldowns.get(command.name);
-	const cooldownAmount = (command.cooldown || 3) * 1000;
-
-	if (timestamps.has(message.author.id)) {
-		const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
-
-		if (now < expirationTime) {
-			const timeLeft = (expirationTime - now) / 1000;
-			return message.reply(`please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${command.name}\` command.`);
-		}
-	}
-
-	timestamps.set(message.author.id, now);
-	setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
-
-	try {
-		command.execute(message, args);
-	} catch (error) {
-		console.error(error);
-		message.reply('there was an error trying to execute that command!');
-	}
+	command(client, interaction, child.options);
 }
-
-const onReady = () => console.log("Tarkovsky is online!")
 
 const main = () => {
 	const client = setupClient(new Discord.Client());
 
-	client.on("ready", onReady.bind(null, client));
-	client.on("message", onMessage.bind(null, client))
+	client.ws.on("INTERACTION_CREATE", onInteraction.bind(null, client))
 
+	client.on("ready", () => console.log("Tarkovsky is online!"));
 	client.login(config.token);
 }
 
---
+++ src/scripts/setup.js
@@ -0,0 +1,48 @@
+const { DiscordInteractions } = require("slash-commands");
+const config = require("../../config.json");
+const slashCommands = require("../../slash-commands.json");
+
+const interaction = new DiscordInteractions({
+	applicationId: config.applicationId,
+	authToken: config.token,
+	publicKey: config.publicKey
+});
+
+const removeExistingCommands = async () => {
+	console.log('> Clearing existing commands...');
+
+	const commands = await interaction.getApplicationCommands();
+	if (!commands.length) {
+		return;
+	}
+
+	await Promise.all(commands.map(({ id }) =>
+		interaction.deleteApplicationCommand(id)
+	));
+
+	console.log('> Commands cleared!');
+}
+
+const createCommands = async () => {
+	console.log('> Adding commands...');
+
+	const response = await Promise.all(slashCommands.map(command =>
+		interaction.createApplicationCommand(command)
+	));
+
+	if (response[0].errors) {
+		console.dir(response);
+	} else {
+		console.log('> Commands added!');
+	}
+}
+
+
+(async () => {
+	try {
+		await removeExistingCommands();
+		await createCommands();
+	} catch (e) {
+		console.error(e);
+	}
+})();
