86fe494905fb55c0cede38c71638c08498d53c02
João Abreu <jpmcabreu@gmail.com>
2021-04-13 09:45:55 +0100

    Implement initial pass on data persistency

--- src/commands/team-kill/report.js
+++ src/commands/team-kill/report.js
@@ -1,5 +1,21 @@
-module.exports = (client, { id, token }) => {
+const getDatabaseInstance = require("../../utils/getDatabaseInstance");
+
+module.exports = (client, { id, token }, options) => {
+	const report = options.reduce((acc, option) => (
+		{ ...acc, [option.name]: option.value }
+	), {})
+
+	report.timestamp = Date.now();
+
+	getDatabaseInstance()
+		.get("teamkills")
+		.push(report)
+		.write();
+
 	client.api.interactions(id, token).callback.post({
-		data: { type: 4, data: { content: "What?" } },
+		data: {
+			type: 4,
+			data: { content: "Report added, cheeki breeki!" }
+		},
 	});
 };
---
+++ src/utils/getDatabaseInstance.js
@@ -0,0 +1,11 @@
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
+
+module.exports = () => {
+	const adapter = new FileSync('db.json')
+	const db = low(adapter)
+
+	db.defaults({ teamkills: [] }).write();
+
+	return db;
+}
